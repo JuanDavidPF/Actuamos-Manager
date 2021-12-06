7538646987c386d99f4721de65588bf4ca70b93d
Juan David Payán <JuanDavidPF@gmail.com>
2021-12-06 17:53:10 -0500

    started working on the main panel

--- src/router.js
+++ src/router.js
@@ -1,9 +1,8 @@
 //link
 let baseLink = "";
 let linkRoute = "";
-let projectRoute = "";
 
-function AnalyzeLink() {
+const AnalyzeLink = () => {
   let link = window.location.href;
   if (!link.includes("#")) {
     baseLink = link + "#";
@@ -13,16 +12,10 @@ function AnalyzeLink() {
     linkSegments = link.split("#");
     baseLink = linkSegments[0] + "#";
     linkRoute = linkSegments[1];
-    projectRoute = "";
-    if (linkRoute.includes("projects")) {
-      let localRoute = linkRoute.split("/");
-      linkRoute = "/" + localRoute[1];
-      if (localRoute[2]) projectRoute = localRoute[2];
-    }
   }
-} //closes Analyzelink method;
+}; //closes Analyzelink method;
 
-function Router() {
+const Router = () => {
   AnalyzeLink();
   currentScreen = linkRoute;
   switch (linkRoute) {
@@ -48,17 +41,18 @@ function Router() {
       Redirect("/404");
       break;
   } //closes Router switch
-} //closes Router method
+}; //closes Router method
 
-function Redirect(path) {
+const Redirect = (path) => {
   window.location.href = baseLink + path;
-} //closes Redirect method
+}; //closes Redirect method
 
-function Refresh() {
+const Refresh = () => {
   Router();
-} //closes Redirect method
+}; //closes Redirect method
 
-window.onhashchange = function () {
+window.onhashchange = () => {
   Router();
 }; //closes onHasChange event
+
 Router();
