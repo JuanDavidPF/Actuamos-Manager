184ca6a4f5a0c1ddcb134957dde87dcb6e7401db
Juan David Payán <JuanDavidPF@gmail.com>
2021-12-08 02:45:08 -0500

    Started working on edit playlist modal

--- src/playlistScreen.js
+++ src/playlistScreen.js
@@ -2,18 +2,64 @@ const playListScreen = document.querySelector("#playlistScreen");
 const playListTitle = playListScreen.querySelector(".title");
 const playListCover = playListScreen.querySelector(".cover");
 
-const FetchPlaylist = (playlistID) => {
+const editThumbnailBTN = playListCover.querySelector(".editButton");
+const editPlayListModal = playListScreen.querySelector(".editPlayListModal");
+const editPlayListModalBtn = editPlayListModal.querySelector(".closeModalBtn");
+
+let playlistID = {};
+let playlistData = {};
+
+const FetchPlaylist = (id) => {
   loadingIndicator.classList.remove("hidden");
   db.collection("Playlists")
-    .doc(playlistID)
+    .doc(id)
     .get()
     .then((doc) => {
       loadingIndicator.classList.add("hidden");
       if (doc.exists) {
+        playlistID = doc.id;
+        playlistData = doc.data();
+
         playListTitle.innerHTML = doc.data().title;
         playListCover.style.backgroundImage = `url(${doc.data().thumbnail})`;
       } else {
         Redirect("404");
       }
+    });
+};
+
+editThumbnailBTN.addEventListener("click", () => {
+  editPlayListModal.classList.remove("hidden");
+});
+
+editPlayListModalBtn.addEventListener("click", () => {
+  editPlayListModal.classList.add("hidden");
+});
+
+const EditTitle = (title) => {
+  db.collection("Playlists")
+    .doc(playlistID)
+    .set(
+      {
+        title: title,
+      },
+      { merge: true }
+    )
+    .then(() => {
+      playListTitle.innerHTML = title;
+    });
+};
+
+const ChanchedThumbnail = (thumbnailURL) => {
+  db.collection("Playlists")
+    .doc(playlistID)
+    .set(
+      {
+        title: title,
+      },
+      { merge: true }
+    )
+    .then(() => {
+      playListTitle.innerHTML = title;
     });
 };
